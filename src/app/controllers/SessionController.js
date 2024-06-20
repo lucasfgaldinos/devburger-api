df19b490a3f97ff3dec70735f8fc376f611ce6d0
lucasfgaldinos <lucasfgaldinos@outlook.com>
2024-06-20 17:10:45 -0300

    first commit

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,59 @@
+import * as Yup from 'yup'
+import jwt from 'jsonwebtoken'
+
+import User from '../models/User.js'
+import authConfig from '../../config/auth.js'
+
+class SessionController {
+    async store(req, res) {
+        const schema = Yup.object({
+            email: Yup.string().email().required(),
+            password: Yup.string().min(6).required()
+        })
+
+        const isValid = await schema.isValid(req.body)
+
+        const emailOrPasswordIncorrect = () => {
+            return res.status(401).json({ error: 'Make sure your email or password are correct' })
+        }
+
+        if (!isValid) {
+            return emailOrPasswordIncorrect()
+        }
+
+        const { email, password } = req.body
+
+        const user = await User.findOne({
+            where: {
+                email
+            }
+        })
+
+        if (!user) {
+            return emailOrPasswordIncorrect()
+        }
+
+        const isSamePassword = await user.checkPassword(password)
+
+        if (!isSamePassword) {
+            return emailOrPasswordIncorrect()
+        }
+
+        return res.status(201).json({
+            id: user.id,
+            name: user.name,
+            email,
+            admin: user.admin,
+            token: jwt.sign(
+                {
+                    id: user.id,
+                    name: user.name,
+                },
+                authConfig.secret,
+                { expiresIn: authConfig.expiresIn }
+            )
+        })
+    }
+}
+
+export default new SessionController()
---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken"
+import authConfig from "../../config/auth.js"
+
+const authMiddlewares = (req, res, next) => {
+    const authToken = req.headers.authorization
+
+    if (!authToken) {
+        return res.status(401).json({ error: 'Token not provided' })
+    }
+
+    const token = authToken.split(' ').at(1)
+
+    try {
+        jwt.verify(token, authConfig.secret, (err, decoded) => {
+            if (err) {
+                throw new Error()
+            }
+
+            req.userId = decoded.id
+            req.userName = decoded.name
+        })
+    } catch (err) {
+        return res.status(401).json({ error: 'Token is invalid' })
+    }
+
+    return next()
+}
+
+export default authMiddlewares
